cf6b8ee933665dc07c08ac04d1381e58c62a845a
booster12-boop <tranduchuy18112017@gmail.com>
2025-05-21 13:01:56 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -36,4 +36,5 @@ def send_email():
         return f'❌ Gửi thất bại: {e}'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
